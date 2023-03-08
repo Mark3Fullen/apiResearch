1242e2e558f0b13502f31323a6584dcefb32332b
Mark <mark3fullen@gmail.com>
2023-03-08 14:56:07 -0500

    redid route

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: ".env" });
 
-const itemRouter = require('./routes/Item');
 const Item = require('./models/Item');
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', itemRouter);
-
 connectToDB()
     .then(() => {
+        app.get('/itemget', async (req, res) => {
+            try {
+              const items = await Item.find();
+              res.status(200).json(items);
+            } catch (err) {
+              console.error('Failed to get items', err);
+              res.status(500).send('Failed to get items');
+            }
+          });
+          
         app.listen(process.env.PORT, () => {
             console.log(`Server listening on port ${process.env.PORT}`);
         });
